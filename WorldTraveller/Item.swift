ee9544f343c5033a8208ce640d72d14b8a4e2295
berksarikayaa <berksarikaaya@gmail.com>
2025-01-19 23:53:00 +0300

    Initial Commit

---
+++ WorldTraveller/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WorldTraveller
+//
+//  Created by Buket Bayhan on 19.01.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WorldTravellerTests/WorldTravellerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravellerTests.swift
+//  WorldTravellerTests
+//
+//  Created by Buket Bayhan on 19.01.2025.
+//
+
+import Testing
+@testable import WorldTraveller
+
+struct WorldTravellerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
