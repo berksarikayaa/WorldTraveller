d3ee65514bbd8650426b7ee8550c95b9f4edff7b
berksarikayaa <berksarikaaya@gmail.com>
2025-01-20 10:52:17 +0300

    Map Button
    20.01.24
    - Added Map Button

--- WorldTraveller/WorldTravellerApp.swift
+++ WorldTraveller/WorldTravellerApp.swift
@@ -10,23 +10,25 @@ import SwiftData
 
 @main
 struct WorldTravellerApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ContentView()
+                    .tabItem {
+                        HStack {
+                            Image(systemName: "thermometer")
+                            Text("Conversion")
+                        }
+                    }
+                MapView()
+                    .tabItem {
+                        HStack {
+                            Image(systemName: "map")
+                            Text("Map")
+                        }
+                    }
+            }
+            .accentColor(.purple)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
